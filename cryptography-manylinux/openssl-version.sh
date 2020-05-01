1dc2d44789afa8039496ad6e3ab2199a6bc8488b
Paul Kehrer <paul.l.kehrer@gmail.com>
2020-04-30 21:51:10 -0500

    de-duplicate openssl build flags

--- cryptography-manylinux/openssl-version.sh
+++ cryptography-manylinux/openssl-version.sh
@@ -1,2 +1,3 @@
 export OPENSSL_VERSION="openssl-1.1.1g"
 export OPENSSL_SHA256="ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46"
+export OPENSSL_BUILD_FLAGS="no-ssl3 no-ssl3-method no-zlib enable-ec_nistp_64_gcc_128 no-shared no-comp no-dynamic-engine"

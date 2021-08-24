6839459c8e3e665ee9268929df15daacc34a33a5
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-08-24 11:00:33 -0400

    update OpenSSL to 1.1.1l (#366)
    * update OpenSSL to 1.1.1l
    
    * might help if it was actually an L

--- cryptography-manylinux/openssl-version.sh
+++ cryptography-manylinux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-1.1.1k"
-export OPENSSL_SHA256="892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+export OPENSSL_VERSION="openssl-1.1.1l"
+export OPENSSL_SHA256="0b7a3e5e59c34827fe0c3a74b7ec8baef302b98fa80088d7f9153aa16fa76bd1"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"

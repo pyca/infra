5f1d5da5ca2f6de40832cd36e4168d07ca8a06d9
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-03-14 10:59:37 +0800

    windows server 2022/msvc 2022 (#392)

--- windows/openssl/build_openssl.bat
+++ windows/openssl/build_openssl.bat
@@ -3,8 +3,8 @@ SET BUILDARCH=%1
 cd openssl-1*
 
 if "%BUILDARCH%" == "win32" (
-    CALL ..\windows\openssl\build_openssl_win32_2019.bat
+    CALL ..\windows\openssl\build_openssl_win32.bat
 ) else (
-    CALL ..\windows\openssl\build_openssl_win64_2019.bat
+    CALL ..\windows\openssl\build_openssl_win64.bat
 )
 if %errorlevel% neq 0 exit /b %errorlevel%

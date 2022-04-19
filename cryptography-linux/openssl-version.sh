4966a57962b0ba397bf429747792438a2a4cda1b
Paul Kehrer <paul.l.kehrer@gmail.com>
2022-04-19 21:50:37 +0200

    use no-module to have libcrypto.a contain the legacy provider as well (#402)

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -2,5 +2,5 @@ export OPENSSL_VERSION="openssl-3.0.2"
 export OPENSSL_SHA256="98e91ccead4d4756ae3c9cde5e09191a8e586d9f4d50838e7ec09d6411dfdb63"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"

725386290cb6e565bb4424ed2f050ab673a7b4af
Alex Gaynor <alex.gaynor@gmail.com>
2022-06-26 09:20:32 -0400

    Refactor windows build files to simplify

--- windows/openssl/build_openssl.bat
+++ windows/openssl/build_openssl.bat
@@ -4,8 +4,24 @@ SET BUILDARCH=%1
 cd openssl-*
 
 if "%BUILDARCH%" == "win32" (
-    CALL ..\windows\openssl\build_openssl_win32.bat
+    call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x86
+    call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
+    SET PATH="%PATH%;C:\Program Files\NASM"
+
+    perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN32
 ) else (
-    CALL ..\windows\openssl\build_openssl_win64.bat
+    call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
+    call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+    SET PATH="%PATH%;C:\Program Files\NASM"
+
+    perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN64A
 )
+
+nmake
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir ..\build
+mkdir ..\build\lib
+move libcrypto.lib ..\build\lib\
+move libssl.lib ..\build\lib\
+move include ..\build\include

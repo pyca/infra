bf3aea41a201d55b328b888a3fc0b7bcf0e7c0af
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-01-19 11:45:47 +0800

    terraform fixes (#443)

--- terraform/cloudinit.tf
+++ terraform/cloudinit.tf
@@ -6,9 +6,20 @@ data "cloudinit_config" "worker" {
   part {
     filename     = "worker.sh"
     content_type = "text/x-shellscript"
+    # https://github.com/oracle-terraform-modules/terraform-oci-oke/blob/9561e1ac70460335ae9e6d03ab73d503185d1d78/modules/oke/cloudinit/worker.template.sh
     content      = <<-EOT
-      #!/bin/bash
-      /usr/libexec/oci-growfs -y
+        #!/bin/bash
+
+        # DO NOT MODIFY
+        curl --fail -H "Authorization: Bearer Oracle" -L0 http://169.254.169.254/opc/v2/instance/metadata/oke_init_script | base64 --decode >/var/run/oke-init.sh
+
+        ## run oke provisioning script
+        bash -x /var/run/oke-init.sh
+
+        ### adjust block volume size
+        /usr/libexec/oci-growfs -y
+
+        touch /var/log/oke.done
     EOT
   }
 

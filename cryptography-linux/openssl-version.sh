c8eb8326657c5a6742269a78b437ccca34eac6a3
Alex Gaynor <alex.gaynor@gmail.com>
2024-07-20 11:53:35 -0400

    OpenSSL 3.3.1 (#562)
    * OpenSSL 3.3.0
    
    * Update openssl-version.sh
    
    * Update openssl-version.sh

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,6 +1,6 @@
-export OPENSSL_VERSION="openssl-3.2.2"
-export OPENSSL_SHA256="197149c18d9e9f292c43f0400acaba12e5f52cacfe050f3d199277ea738ec2e7"
+export OPENSSL_VERSION="openssl-3.3.1"
+export OPENSSL_SHA256="777cd596284c883375a2a7a11bf5d2786fc5413255efab20c50d6ffe6d020b7e"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp"
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"

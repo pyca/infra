3a92c5cba96d920617d9c586ebc64abc96ac08ac
Paul Kehrer <paul.l.kehrer@gmail.com>
2024-09-03 12:18:07 -0700

    openssl 3.3.2 (#612)
    * openssl 3.3.2
    
    * fix paths for mac/win as well
    
    * vendor a patch to work around the openssl 3.3.2 perl issue

--- cryptography-linux/install_openssl.sh
+++ cryptography-linux/install_openssl.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -xe
 
-OPENSSL_URL="https://www.openssl.org/source/"
+OPENSSL_URL="https://github.com/openssl/openssl/releases/download"
 source /root/openssl-version.sh
 
-curl -#LO "${OPENSSL_URL}/${OPENSSL_VERSION}.tar.gz"
+curl -#LO "${OPENSSL_URL}/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz"
 echo "${OPENSSL_SHA256}  ${OPENSSL_VERSION}.tar.gz" | sha256sum -c -
 tar zxf ${OPENSSL_VERSION}.tar.gz
 pushd ${OPENSSL_VERSION}
+# Patch to work around OpenSSL 3.3.2 requiring a newer perl than manylinux2014
+if [ -f /etc/redhat-release ] && grep -q "CentOS Linux release 7" /etc/redhat-release; then
+    git apply ../list-util-pairs-25367.patch
+fi
 ./config $OPENSSL_BUILD_FLAGS --prefix=/opt/pyca/cryptography/openssl --openssldir=/opt/pyca/cryptography/openssl
 make depend
 make -j4

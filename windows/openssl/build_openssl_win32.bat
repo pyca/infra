3b3ec81ac9f84bfb81f83327f330b9e3027d3679
Paul Kehrer <paul.l.kehrer@gmail.com>
2024-11-28 13:54:26 -0800

    try using jom for parallel windows compilation (#649)
    * try using jom for parallel windows compilation
    
    * try removing -j4

--- windows/openssl/build_openssl_win32.bat
+++ windows/openssl/build_openssl_win32.bat
@@ -1,9 +1,10 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x86
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
 SET PATH=%PATH%;C:\Program Files\NASM
+SET CL=/FS
 
 perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN32
-nmake
+jom
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir ..\build

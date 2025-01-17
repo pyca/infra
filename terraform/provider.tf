aa615fc594be195310776998eff17f19b64897fc
Alex Gaynor <alex.gaynor@gmail.com>
2025-01-17 18:09:58 -0500

    At long last our national terraform/k8s nightmare is over

--- terraform/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "oci" {
-  fingerprint  = var.api_fingerprint
-  private_key  = file(var.api_private_key_path)
-  region       = var.region
-  tenancy_ocid = var.tenancy_id
-  user_ocid    = var.user_id
-}

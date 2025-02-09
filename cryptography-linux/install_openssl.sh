519b16bd0803ea22dbd0a4525c641060581c0215
Alex Gaynor <alex.gaynor@gmail.com>
2025-02-09 12:25:34 -0500

    build armv7l images (#669)
    * build armv7l images
    
    * Update build-docker-images.yml
    
    * Don't use `enable-ec_nistp_64_gcc_128` on armv7l
    
    * hack?
    
    * We are an armv4 build

--- cryptography-linux/install_openssl.sh
+++ cryptography-linux/install_openssl.sh
@@ -8,7 +8,13 @@ curl -#LO "${OPENSSL_URL}/${OPENSSL_VERSION}/${OPENSSL_VERSION}.tar.gz"
 echo "${OPENSSL_SHA256}  ${OPENSSL_VERSION}.tar.gz" | sha256sum -c -
 tar zxf ${OPENSSL_VERSION}.tar.gz
 pushd ${OPENSSL_VERSION}
-./config $OPENSSL_BUILD_FLAGS --prefix=/opt/pyca/cryptography/openssl --openssldir=/opt/pyca/cryptography/openssl
+BUILD_FLAGS="$OPENSSL_BUILD_FLAGS"
+# Can't use `$(uname -m) = "armv7l"` because that returns what kernel we're
+# using, and we build for armv7l with an ARM64 host.
+if [ "$(readelf -h /proc/self/exe | grep -o 'Machine:.* ARM')" ]; then
+    BUILD_FLAGS="$OPENSSL_BUILD_FLAGS_ARMV7L"
+fi
+./config $BUILD_FLAGS --prefix=/opt/pyca/cryptography/openssl --openssldir=/opt/pyca/cryptography/openssl
 make depend
 make -j4
 # avoid installing the docs
--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -3,4 +3,5 @@ export OPENSSL_SHA256="e15dda82fe2fe8139dc2ac21a36d4ca01d5313c75f99f46c4e8a27709
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
+export OPENSSL_BUILD_FLAGS_ARMV7L="linux-armv4 ${OPENSSL_BUILD_FLAGS_WINDOWS}"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"

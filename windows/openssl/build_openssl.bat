afd699c3c5023f9bb43464abf6bdd06b7126b2ba
Alex Gaynor <alex.gaynor@gmail.com>
2025-04-15 18:22:44 -0400

    Attempt to build OpenSSL for arm64 on Windows (#686)

--- windows/openssl/build_openssl.bat
+++ windows/openssl/build_openssl.bat
@@ -10,11 +10,16 @@ if "%BUILDARCH%" == "win32" (
     CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
 
     perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN32
-) else (
+) else if "%BUILDARCH%" == "win64" (
     CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64
     CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 
     perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN64A
+) else if "%BUILDARCH%" == "arm64" (
+    CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=arm64
+    CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" arm64
+
+    perl Configure %OPENSSL_BUILD_FLAGS_WINDOWS% VC-WIN64-ARM
 )
 
 jom

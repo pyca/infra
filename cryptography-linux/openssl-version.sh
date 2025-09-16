cd0976eefd6c2ce76d09a77fa7e535aea72ed217
Paul Kehrer <paul.l.kehrer@gmail.com>
2025-09-16 09:02:23 -0700

    update openssl version to 3.5.3 (#715)
    * update openssl version to 3.5.3
    
    * Add perl-Time-Piece to Dockerfile dependencies
    
    * Add perl-Time-Piece package installation for aarch64

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-3.5.2"
-export OPENSSL_SHA256="c53a47e5e441c930c3928cf7bf6fb00e5d129b630e0aa873b08258656e7345ec"
+export OPENSSL_VERSION="openssl-3.5.3"
+export OPENSSL_SHA256="c9489d2abcf943cdc8329a57092331c598a402938054dc3a22218aea8a8ec3bf"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"

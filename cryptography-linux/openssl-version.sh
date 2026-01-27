fc4690f293b7d23585e0579962622e550d154194
Paul Kehrer <paul.l.kehrer@gmail.com>
2026-01-27 13:18:45 -0600

    openssl 3.5.5 (#734)
    * openssl 3.5.5
    
    * Increase Docker image build timeout to 20 minutes

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-3.5.4"
-export OPENSSL_SHA256="967311f84955316969bdb1d8d4b983718ef42338639c621ec4c34fddef355e99"
+export OPENSSL_VERSION="openssl-3.5.5"
+export OPENSSL_SHA256="b28c91532a8b65a1f983b4c28b7488174e4a01008e29ce8e69bd789f28bc2a89"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"

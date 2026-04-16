0564f39bd41838d920814e22fb5de91518d52232
Paul Kehrer <paul.l.kehrer@gmail.com>
2026-04-16 10:20:02 -0500

    openssl 4.0.0
    this removes sslv3 support as well as engines so we can remove those
    from our build flags

--- cryptography-linux/openssl-version.sh
+++ cryptography-linux/openssl-version.sh
@@ -1,7 +1,7 @@
-export OPENSSL_VERSION="openssl-3.5.6"
-export OPENSSL_SHA256="deae7c80cba99c4b4f940ecadb3c3338b13cb77418409238e57d7f31f2a3b736"
+export OPENSSL_VERSION="openssl-4.0.0"
+export OPENSSL_SHA256="c32cf49a959c4f345f9606982dd36e7d28f7c58b19c2e25d75624d2b3d2f79ac"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
-export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-module no-comp no-dynamic-engine no-apps no-docs no-sm2-precomp no-atexit"
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-zlib no-shared no-module no-comp no-apps no-docs no-sm2-precomp no-atexit"
 export OPENSSL_BUILD_FLAGS_ARMV7L="linux-armv4 ${OPENSSL_BUILD_FLAGS_WINDOWS}"
 export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
